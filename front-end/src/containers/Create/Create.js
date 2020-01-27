34187cec09e89783fc2f3b9bbc4767236fe61190
Omer Demirkan <omerfarukpiano@gmail.com>
2020-01-27 15:50:26 -0800

    import all needed libraries fo material-ui datepicker

--- front-end/src/containers/Create/Create.js
+++ front-end/src/containers/Create/Create.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from './Create.module.css';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
 
 export default function Create() {
+    const [selectedDate, setSelectedDate] = useState(new Date('2014-08-18T21:11:54'));
+
+    const handleDateChange = date => {
+        setSelectedDate(date);
+    };
     return <div>
-        <p>Create</p>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+            disableToolbar
+            variant="inline"
+            format="MM/dd/yyyy"
+            margin="normal"
+            id="date-picker-inline"
+            label="Date picker inline"
+            value={selectedDate}
+            onChange={handleDateChange}
+            KeyboardButtonProps={{
+                'aria-label': 'change date',
+            }}
+            />
+        </MuiPickersUtilsProvider>
         
     </div>
 }
